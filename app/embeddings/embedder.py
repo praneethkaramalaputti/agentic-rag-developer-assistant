ca1b5cd675fe15afbd9cc011ec5e13ed8a0b7c02
Praneeth Guptha Karamalaputti <praneethgupthakaramalaputti@Praneeths-MacBook-Pro.local>
2026-04-04 14:50:06 -0400

    Add embedding module

---
+++ app/embeddings/embedder.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def embed_texts(texts):
+    return model.encode(texts).tolist()
+
+def embed_query(query: str):
+    return model.encode([query])[0].tolist()
